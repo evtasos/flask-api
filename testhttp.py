59d6ab16d586cd48100e776246532f61a9457ba4
evtasos <evtasos1@gmail.com>
2023-01-12 09:04:27 +0200

    change http requests

--- testhttp.py
+++ testhttp.py
@@ -1,10 +1,12 @@
 import requests
 
-url = "http://127.0.0.1:5000/upload"
-file_path = r"C:\Users\plhroforikh-1\Desktop\thesis\flask-api\filespdf\testflask.docx"
+# url = "http://127.0.0.1:5000/upload"
+# file_path = r"C:\Users\plhroforikh-1\Desktop\thesis\flask-api\filespdf\testflask.docx"
 
-with open(file_path, 'rb') as f:
-    file = {'file': (file_path, f)}
-    response = requests.post(url, files=file)
+# with open(file_path, 'rb') as f:
+#     file = {'file': (file_path, f)}
+#     response = requests.post(url, files=file)
+    
+response = requests.get("http://127.0.0.1:5000/list")
 
 print(response.json())
