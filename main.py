c0c73f839bf274150fe76ab265b4bffe7b4754e8
evtasos <evtasos1@gmail.com>
2023-01-13 14:07:08 +0200

    code improvements

--- main.py
+++ main.py
@@ -8,17 +8,14 @@ from datetime import datetime
 
 UPLOAD_FOLDER = 'archive'
 ALLOWED_EXTENSIONS = {'pdf', 'docx'}
-
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
 app = Flask(__name__, template_folder= '.')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
 
 # Route for uploading a new file
 @app.route('/upload', methods=['POST'])
@@ -40,7 +37,18 @@ def upload_files():
 @app.route('/create', methods=['POST'])
 def create_file():
     data = request.get_json()
-    
+    #check if all parameters are given
+    if not all(key in data for key in ('filename', 'recipient', 'am', 'year')):
+        return jsonify({"error": "Missing required parameters"}), 400
+    # check if the recipient field is a string
+    if not isinstance(data.get('recipient'), str):
+        return jsonify({"error": "recipient parameter should be a string"}), 400
+    # check if the am field is an integer
+    if not isinstance(data.get('am'), int):
+        return jsonify({"error": "am parameter should be an integer"}), 400
+    # check if the year field is an integer
+    if not isinstance(data.get('year'), int):
+        return jsonify({"error": "year parameter should be an integer"}), 400
     now = datetime.now()
     dt_string = now.strftime("%d/%m/%Y")
 
@@ -50,6 +58,8 @@ def create_file():
         am = data.get('am')
         year = data.get('year')
 
+        if not os.path.isfile(template):
+            return jsonify({"error": f"{template} not found"}), 404
         doc = DocxTemplate(template)
         context = { 'recipient' : recipient, 'am' : am, 'year' : year}
         doc.render(context)
@@ -75,14 +85,15 @@ def download_file(filename):
 def modify_file(filename):
     # Get the new file data from the request
     file_data = request.get_json()
+    file_path = os.path.join(UPLOAD_FOLDER, filename)
     # Check if the file exists
-    if os.path.exists(filename):
+    if os.path.exists(file_path):
         # Write the new file data to the server
-        with open(filename, "w") as f:
+        with open(file_path, "w") as f:
             f.write(file_data["content"])
-        return jsonify({"success": "File modified successfully"})
+        return jsonify({"success": "File modified successfully"}), 200
     else:
-        return jsonify({"error": "File not found"})
+        return jsonify({"error": "File not found"}), 404
 
 # Route for listing all files
 @app.route('/list', methods=['GET'])
@@ -94,11 +105,16 @@ def list_files():
 @app.route('/delete/<filename>', methods=['DELETE'])
 def delete_file(filename):
     file_path = os.path.join(UPLOAD_FOLDER, filename)
+    # Check if the file exists
     if os.path.isfile(file_path):
         os.remove(file_path)
         return jsonify({"success": "File deleted successfully"}), 200
     else:
         return jsonify({"error": "File not found"}), 400
-    
+
+# Route for handling teh case when the endpoint is not found
+@app.errorhandler(404)
+def page_not_found(e):
+    return jsonify({"error": "Endpoint not found"}), 404    
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
--- testhttprequests.py
+++ testhttprequests.py
@@ -1,11 +1,11 @@
 import requests
 
-# #upload
-# url = "http://127.0.0.1:5000/upload"
-# file_path = r"C:\Users\plhroforikh-1\Desktop\thesis\lorem.docx"
-# with open(file_path, 'rb') as f:
-#     file = {'file': (file_path, f)}
-#     response = requests.post(url, files=file)
+#upload
+url = "http://127.0.0.1:5000/upload"
+file_path = r"C:\Users\plhroforikh-1\Desktop\thesis\lorem2.docx"
+with open(file_path, 'rb') as f:
+    file = {'file': (file_path, f)}
+    response = requests.post(url, files=file)
     
 #list
 #response = requests.get("http://127.0.0.1:5000/list")
@@ -23,7 +23,7 @@ import requests
 # response = requests.post("http://127.0.0.1:5000/create", json=data)
 
 #download
-response = requests.get("http://127.0.0.1:5000/
+#response = requests.get("http://127.0.0.1:5000/download/lorem.docx")
 
 print(response.json())
 
